5051376edc5d13261cbd86854fe8526ce5666fdb
Yomguithereal <guillaumeplique@gmail.com>
2017-11-13 11:21:44 +0100

    Moving folder

--- scripts/ideo2017/most-frequent-words.js
+++ scripts/ideo2017/most-frequent-words.js
@@ -6,6 +6,9 @@ const request = require('request'),
       path = require('path'),
       fs = require('fs');
 
+const MultiSet = require('mnemonist/multi-set'),
+      MultiMap = require('mnemonist/multi-map');
+
 const candidates = require('./candidates');
 
 const url = candidate => `http://patrimeph.ensea.fr/RST/iramu.php?CorpusName=Corpus%2F${candidate}.txt&candidat=${candidate}&stat=`;
@@ -26,7 +29,7 @@ async.mapSeries(candidates, (candidate, next) => {
       .map(row => {
         return {
           word: row[0],
-          frequency: row[1],
+          frequency: +row[1],
           type: row[2]
         };
       });
@@ -37,9 +40,33 @@ async.mapSeries(candidates, (candidate, next) => {
   if (err)
     return console.error(err);
 
+  const words = new MultiSet(),
+        wordsToCandidates = new MultiMap(),
+        types = new Map();
+
+  data.forEach((candidateData, i) => {
+    candidateData.forEach(row => {
+      types.set(row.word, row.type);
+      words.add(row.word, row.frequency);
+      wordsToCandidates.set(row.word, candidates[i]);
+    });
+  });
+
+  const aggregated = Array.from(types.keys()).map(word => {
+    return {
+      word,
+      frequency: words.multiplicity(word),
+      candidatesCount: wordsToCandidates.multiplicity(word),
+      candidates: wordsToCandidates.get(word).join('|'),
+      type: types.get(word)
+    };
+  });
+
   const folder = path.join('.output', 'most-frequent-words');
 
   mkdirp.sync(folder);
+
+  fs.writeFileSync(path.join(folder, `aggregated.csv`), artoo.writers.csv(aggregated));
 
   data.forEach((candidateData, i) => {
     fs.writeFileSync(path.join(folder, `${candidates[i]}.csv`), artoo.writers.csv(candidateData));
