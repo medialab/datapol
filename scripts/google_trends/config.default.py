f3eb5def57c5b62a2545e0ebd66a5d4f6f4508ed
samaingw <samain.gwenael@laposte.net>
2017-11-23 17:25:33 +0100

    First steps in GTrends API

---
+++ scripts/google_trends/config.default.py
@@ -0,0 +1,6 @@
+SERVER = 'https://www.googleapis.com'
+
+API_VERSION = 'xxxx'
+DISCOVERY_URL_SUFFIX = 'xxxxxx' + API_VERSION + 'xxx'
+DISCOVERY_URL = SERVER + DISCOVERY_URL_SUFFIX
+API_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxxx'
