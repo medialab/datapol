0e3b1f3b91b0405c249bbded4f368d992f1373e5
Yomguithereal <guillaumeplique@gmail.com>
2017-11-29 16:13:27 +0100

    Fixing source file

--- scripts/INA/format_sources.py
+++ scripts/INA/format_sources.py
@@ -1,8 +1,33 @@
 import csv
 import json
+import requests
+from config import SERVER_URL
+
+SEARCH_URL = '%s/twitter.dlweb/ppc/ws/search' % SERVER_URL
+
+def get_first_tweet(screen_name):
+    args = {
+        'from': 0,
+        'from_date': '',
+        'hashtags': '',
+        'index': 'twitter_search_lab02',
+        'lang': '',
+        'mentions': '',
+        'query': 'user.screen_name:"%s"' % screen_name,
+        'quote': '-1',
+        'retweet': '-1',
+        'size': 1,
+        'sort_field': 'created_at',
+        'sort_type': 'asc',
+        'to_date': '',
+        'users': ''
+    }
+
+    res = requests.post(SEARCH_URL, data=json.dumps(args))
+    return res.json()
 
 with open('./sources.jsonl', 'r') as sf, open('./sources.csv', 'w') as of:
-    writer = csv.DictWriter(of, fieldnames=['account', 'description', 'tags'])
+    writer = csv.DictWriter(of, fieldnames=['id', 'account', 'description', 'tags'])
     writer.writeheader()
 
     for line in sf.readlines():
@@ -14,8 +39,17 @@ with open('./sources.jsonl', 'r') as sf, open('./sources.csv', 'w') as of:
         if data['type'] != 'timeline':
             continue
 
-        writer.writerow({
+        row = {
+            'id': '',
             'account': data['key'],
             'description': data['description'],
             'tags': '|'.join(data['extended_data']['tags']) if 'extended_data' in data else ''
-        })
+        }
+
+        if row['tags']:
+            tweet = get_first_tweet(row['account'])
+
+            if len(tweet['hits']):
+                row['id'] = tweet['hits'][0]['_source']['user']['id']
+
+        writer.writerow(row)
