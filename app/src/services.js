d8dfcfad5477bd79d4e31dcb6be875875b5268e7
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2018-06-18 18:10:13 +0200

    rework dataset descriptions

--- app/src/services.js
+++ app/src/services.js
@@ -10,109 +10,109 @@ angular.module('app.services', [])
     ns.datasets = [
       {
       	id: 'corpus-candidats-linkfluence',
-        title: 'Corpus “Candidats présidentielle 2017” sur Linkfluence Radarly',
+        title: 'Candidats sur les réseaux sociaux',
         licence: 'confidentiel',
         authors: 'Linkfluence : Guilhem Fouetillou',
         contact: 'benoit.tabutiaux@linkfluence.com'
       },{
       	id: 'extractions-corpus-linkfluence',
-        title: 'Extractions du corpus Radarly Linkfluence',
+        title: 'Extractions du corpus Radarly',
         licence: 'confidentiel',
         authors: 'Linkfluence : Guilhem Fouetillou',
         contact: 'benoit.tabutiaux@linkfluence.com'
       },{
       	id: 'politoscope',
-        title: 'Données Twitter de la campagne présidentielle 2017 ISC-PIF Politoscope.org',
+        title: 'Twitter de la campagne présidentielle 2017',
         licence: 'confidentiel',
         authors: 'Institut des Systèmes Complexes de Paris Ile-de-France (ISC-PIF) sous la responsabilité de David Chavalarias. David Chavalarias (CNRS/EHESS, ISC-PIF/CAMS), responsable scientifique, data-mining & visualisations. Maziyar Panahi (CNRS, ISC-PIF) : responsable de l’infrastructure Big Data, chef de projet Multivac. Noé Gaumont (CNRS/EHESS, CAMS/ISC-PIF) : analyse des réseaux. Alexandre Delanoë (CNRS, ISC-PIF) : fouille de texte, chef de projet Gargantext.',
         contact: 'david.chavalarias@iscpif.fr'
       },{
       	id: 'sitotheque-bib',
-        title: 'Sitothèque élections 2017 de la Bibliothèque de Sciences Po',
+        title: 'Sitothèque élections 2017',
         licence: 'ODBL',
         authors: 'CEVIPOF : Diégo Antolinos-Basso, Odile Gaultier-Voituriez et Thierry Vedel (chercheur référent). Médialab : Audrey Baneyx, Mathieu Jacomy, Benjamin Ooghe-Tabanou. Bibliothèque : Laurent Bajon, Anita Beldiman-Moore, Anne L’Hôte, Zohra Mechri, Meryam Maizi, Cynthia Pedroja, Myriam Tazi',
         contact: 'cynthia.pedroja@sciencespo.fr',
         link: 'http://corpusweb.sciencespo.fr/#/en/election-presidentielle-2017/view/grid'
       },{
       	id: 'ideo-2017',
-        title: 'Tweets des candidats à la présidentielle, par IDEO 2017',
+        title: 'Tweets des candidats à la présidentielle',
         licence: 'CC-BY-SA',
         authors: 'Laboratoires producteurs : AGORA - Laboratoire AGORA (Cergy-Pontoise, France), ETIS - Equipes Traitement de l\'Information et Systèmes (Cergy-Pontoise, France). Soutien institutionnel : Fondation UCP (Cergy-Pontoise, France), CORLI - Consortium CORLI - Corpus, Langues, Interactions ',
         contact: 'julien.longhi@u-cergy.fr',
         link: 'https://www.ortolang.fr/market/corpora/corpus-presidentielle2017'
       },{
       	id: 'crawl-decodex',
-        title: 'Catégories du Décodex (Le Monde et Sciences Po médialab)',
+        title: 'Décodex catégorisé',
         licence: 'ODBL',
         authors: 'Décodex en lui-même : Les Décodeurs (Le Monde). Catégories additionnelles : Mathieu Jacomy (Sciences Po médialab)',
         contact: 'mathieu.jacomy@sciencespo.fr',
         link: 'https://github.com/medialab/datapol/tree/master/data/crawl-decodex'
       },{
       	id: 'google-trends',
-        title: 'Extractions Google Trends de la campagne électorale',
+        title: 'Google Trends de la campagne électorale',
         licence: 'ODBL',
         authors: 'Données Trends : Google. Extraction et sélection : Sciences Po médialab',
         contact: 'medialab@sciencespo.fr',
         link: 'https://github.com/medialab/datapol/tree/master/data/google-trends'
       },{
       	id: 'nos-deputes',
-        title: 'Nos Députés (Regards Citoyens)',
+        title: 'Nos Députés',
         licence: 'ODBL',
         authors: 'Assemblée nationale et Regards Citoyens',
         contact: 'benjamin.ooghe@sciencespo.fr',
         link: 'https://github.com/regardscitoyens/nosdeputes.fr/blob/master/doc/opendata.md'
       },{
       	id: 'elipss',
-        title: 'Réponses aux vagues de questions Dynamob sur le panel Elipss',
+        title: 'Dynamob',
         licence: 'confidentiel',
         authors: 'Vincent TIBERJ et Florent GOUGOU (Centre d\'études européennes, Sciences Po), Équipe Dynamob, Équipe Elipss',
         contact: 'info.cdsp@sciencespo.fr'
       },{
       	id: 'ina',
-        title: 'Archive Twitter INA Elections 2017',
+        title: 'Tweets pendant la campagne',
         licence: 'confidentiel',
         authors: 'Lucien Castex (sourcing), Zeynep Pehlivan (captation, indexation, outil)',
         contact: 'dlweb@ina.fr'
       },{
       	id: 'listic-twitter',
-        title: 'Tweets sur FN/Fachosphère formatés pour traitement lexicographique de Listic',
+        title: 'Tweets FN/Fachosphère',
         licence: 'confidentiel',
         authors: 'Guillaume Cabanac est maître de conférences HDR en informatique à l\'Institut de recherche en informatique de Toulouse (UMR 5505 CNRS), Université 3 - Paul Sabatier. Julien Figeac, porteur du projet, est chargé de recherche au CNRS en sociologie (section 36 et CID 53). Il est membre du LISST de l’Université Toulouse 2. Ophélie Fraisier, est Doctorante, chargée de cours, en Informatique à l\'Institut de recherche en informatique de Toulouse (UMR 5505 CNRS), Université 3 - Paul Sabatier. Xavier Milliner, est assistant de recherche CNRS, LISST de l’Université Toulouse 2 – Jean Jaurès. Pierre Ratinaud est MCF en Sciences de l’Education au département des sciences de l’éducation et de la formation de l’Université de Toulouse 2 – Jean Jaurès et membre du LERASS à l’Université Toulouse 3 – Paul Sabatier. Tristan Salord, est assistant de recherche CNRS, LISST de l’Université Toulouse 2 – Jean Jaurès. Ancien Doctorant en Sociologie, Chargé de projets. Fanny Seffusatti, est assistante de recherche CNRS, LISST de l’Université Toulouse 2 – Jean Jaurès. Nikos Smyrnaios est MCF en Sciences de l’Information et de la Communication et membre du LERASS à l’Université Toulouse 3 – Paul Sabatier.',
         contact: 'julien.figeac@univ-tlse2.fr'
       },{
       	id: 'listic-facebook',
-        title: 'Données Facebook (via Netvizz) sur FN/Fachosphère de Listic',
+        title: 'Facebook FN/Fachosphère',
         licence: 'confidentiel',
         authors: 'Guillaume Cabanac est maître de conférences HDR en informatique à l\'Institut de recherche en informatique de Toulouse (UMR 5505 CNRS), Université 3 - Paul Sabatier. Julien Figeac, porteur du projet, est chargé de recherche au CNRS en sociologie (section 36 et CID 53). Il est membre du LISST de l’Université Toulouse 2. Ophélie Fraisier, est Doctorante, chargée de cours, en Informatique à l\'Institut de recherche en informatique de Toulouse (UMR 5505 CNRS), Université 3 - Paul Sabatier. Xavier Milliner, est assistant de recherche CNRS, LISST de l’Université Toulouse 2 – Jean Jaurès. Pierre Ratinaud est MCF en Sciences de l’Education au département des sciences de l’éducation et de la formation de l’Université de Toulouse 2 – Jean Jaurès et membre du LERASS à l’Université Toulouse 3 – Paul Sabatier. Tristan Salord, est assistant de recherche CNRS, LISST de l’Université Toulouse 2 – Jean Jaurès. Ancien Doctorant en Sociologie, Chargé de projets. Fanny Seffusatti, est assistante de recherche CNRS, LISST de l’Université Toulouse 2 – Jean Jaurès. Nikos Smyrnaios est MCF en Sciences de l’Information et de la Communication et membre du LERASS à l’Université Toulouse 3 – Paul Sabatier.',
         contact: 'julien.figeac@univ-tlse2.fr'
       },{
       	id: 'listic-facebook-urls',
-        title: 'Liste d’URLs partagées sur pages Facebook politisées',
+        title: 'URLs politisées sur Facebook',
         licence: 'ODBL',
         authors: 'Guillaume Cabanac, Julien Figeac, Ophélie Fraisier, Xavier Milliner, Pierre Ratinaud, Tristan Salord, Fanny Seffusatti, Nikos Smyrnaios',
         contact: 'julien.figeac@univ-tlse2.fr',
         link: 'https://github.com/medialab/datapol/tree/master/data/listic-facebook-urls'
       },{
       	id: 'videos-metrics-factory',
-        title: 'Vidéos Candidats Facebook (The Metrics Factory)',
+        title: 'Vidéos Candidats Facebook',
         licence: 'confidentiel',
         authors: 'Rémi Douine est fondateur de The Metrics Factory et enseignant à Sciences-Po, Quentin Gressien est consultant sénior de The Metrics Factory et enseignant à IAE Paris, Erwan Le Nagard est directeur de l’Engagement Labs de The Metrics Factory et enseignant au CELSA, Jérémy Danulot est consultant à The Metrics Factory, Mélissa Dubois est consultante à The Metrics Factory, Ivan Ferret est consultant à The Metrics Factory, Maud Hermosa est consultante à The Metrics Factory, Pierre-Marie Labry est consultant à The Metrics Factory, Yasmine Nennouche est consultante à The Metrics Factory, Aurélie Santos est consultante à The Metrics Factory.',
         contact: 'douine@themetricsfactory.com'
       },{
       	id: 'ens-insoumis',
-        title: 'Echanges des Insoumis sur les réseaux sociaux (ENS)',
+        title: 'Echanges des Insoumis sur les réseaux sociaux',
         licence: 'confidentiel',
         authors: 'Zakarya Després (Lattice), Clément Plancq (Lattice)',
         contact: 'zakarya.despres@gmail.com'
       },{
       	id: 'diasporas-lab',
-        title: 'Titres d’articles consultés sur postes publics (Diasporas Lab)',
+        title: 'Articles consultés depuis postes publics',
         licence: 'confidentiel',
         authors: 'Diasporas Lab : Dana Diminescu et Quentin Lobbé',
         contact: 'dana.diminescu@telecom-paristech.fr'
       },{
       	id: 'tweets-deputes',
-        title: 'Tweets des députés (Sciences Po médialab)',
+        title: 'Tweets des députés',
         licence: 'confidentiel',
         authors: 'Sciences Po médialab',
         contact: 'benjamin.ooghe@sciencespo.fr'
